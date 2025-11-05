7f88093fd1780085864ee6e97604d49d809a8a51
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-11-05 09:09:01 +0100

    Modernize stack, use VLT8

--- frontend/packages/website/src/index.ts
+++ frontend/packages/website/src/index.ts
@@ -1,16 +1,8 @@
 import { ConfigType } from '@plone/registry';
 import installSettings from './config/settings';
-import installSlots from './config/slots';
-
-declare module '@plone/types' {
-  export interface GetSiteResponse {
-    'kitconcept.website.custom_css': string;
-  }
-}
 
 const applyConfig = (config: ConfigType) => {
   installSettings(config);
-  installSlots(config);
   return config;
 };
 
