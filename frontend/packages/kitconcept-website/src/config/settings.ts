7c897ffea7bd3c78cf1440795ae180206cc7063e
Victor Fernandez de Alba <sneridagh@gmail.com>
2026-05-04 14:21:56 +0200

    Better

--- frontend/packages/kitconcept-website/src/config/settings.ts
+++ frontend/packages/kitconcept-website/src/config/settings.ts
@@ -1,6 +1,8 @@
 import { ConfigType } from '@plone/registry';
 
 export default function install(config: ConfigType) {
+  config.settings.supportedLanguages = ['de', 'en'];
+
   // Volto Light Theme Configuration
   config.settings.siteHeader = true;
   config.settings.siteLabel = 'Site';
