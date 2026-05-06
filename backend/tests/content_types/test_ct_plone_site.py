633acfd43875c80b8a41601d84510ff3da486ad5
Víctor Fernández de Alba <sneridagh@gmail.com>
2026-05-06 12:05:22 +0200

    core 2a16 (#27)
    * core 2a16
    
    * Fix tests

--- backend/tests/content_types/test_ct_plone_site.py
+++ backend/tests/content_types/test_ct_plone_site.py
@@ -37,6 +37,7 @@ class TestContentTypeFTI:
             "kitconcept.sticky_menu",
             "plonegovbr.socialmedia.settings",
             "plone.basic",
+            "plone.categorization",
             "plone.relateditems",
             "plone.locking",
             "volto.blocks",
