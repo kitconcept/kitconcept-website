b5d2821c177bd1c24d0e933bbe5530b0cf74cbe1
Victor Fernandez de Alba <sneridagh@gmail.com>
2026-05-15 11:00:30 +0200

    Added cypress tests

---
+++ frontend/cypress/tests/a11y/accordionBlock.cy.js
@@ -0,0 +1,21 @@
+describe('a11y tests', () => {
+  beforeEach(() => {
+    cy.intercept('GET', `/**/*?expand*`).as('content');
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('/');
+    cy.wait('@content');
+  });
+
+  // Accordion Block
+  it('Accordion Block (/features/block/block-accordion)', () => {
+    cy.navigate('/features/block/block-accordion');
+    cy.wait('@content').its('response.statusCode').should('eq', 200);
+    cy.injectAxe();
+    // To prevent color contrast error during css transition
+    cy.wait(200);
+    cy.checkAccessibility();
+  });
+});
