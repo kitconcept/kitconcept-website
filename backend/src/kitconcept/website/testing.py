a49f24ecd3810ca3d010eb44908a0848bf2eeb16
Victor Fernandez de Alba <sneridagh@gmail.com>
2026-05-29 17:22:13 +0200

    Last things standing

--- backend/src/kitconcept/website/testing.py
+++ backend/src/kitconcept/website/testing.py
@@ -1,4 +1,16 @@
 from kitconcept.core.testing.layers import kitconceptDistributionFixture
+from plone.app.robotframework.autologin import AutoLogin
+from plone.app.robotframework.content import Content
+from plone.app.robotframework.genericsetup import GenericSetup
+from plone.app.robotframework.i18n import I18N
+from plone.app.robotframework.mailhost import MockMailHost
+from plone.app.robotframework.quickinstaller import QuickInstaller
+from plone.app.robotframework.remote import RemoteLibraryLayer
+from plone.app.robotframework.server import Zope2ServerRemote
+from plone.app.robotframework.testing import MockMailHostLayer as BaseMailLayer
+from plone.app.robotframework.testing import PloneRobotFixture
+from plone.app.robotframework.testing import WSGI_SERVER_SINGLE_THREADED_FIXTURE
+from plone.app.robotframework.users import Users
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import PloneSandboxLayer
@@ -89,4 +101,116 @@ INTEGRATION_TESTING = IntegrationTesting(
 FUNCTIONAL_TESTING = FunctionalTesting(
     bases=(FIXTURE, WSGI_SERVER_FIXTURE),
     name="kitconcept.websiteLayer:FunctionalTesting",
+)
+
+
+ANSWERS_CONTENT = {
+    **DEFAULT_ANSWERS,
+    "setup_content": False,
+}
+
+
+class ContentFixture(BaseFixture):
+    sites = (("kitconcept-website", ANSWERS_CONTENT),)
+
+
+CONTENT_FIXTURE = ContentFixture()
+
+
+class MockMailHostLayer(BaseMailLayer):
+    defaultBases = (CONTENT_FIXTURE,)
+
+
+MOCK_MAILHOST_FIXTURE = MockMailHostLayer()
+
+
+class RobotFixture(PloneRobotFixture):
+    """Acceptance testing fixture, using robot framework, for kitconcept.website."""
+
+    defaultBases = (MOCK_MAILHOST_FIXTURE,)
+
+    def setUpPloneSite(self, portal):
+        super().setUpPloneSite(portal)
+
+
+ROBOT_FIXTURE = RobotFixture()
+
+REMOTE_LIBRARY_BUNDLE_FIXTURE = RemoteLibraryLayer(
+    bases=(CONTENT_FIXTURE,),
+    libraries=(
+        AutoLogin,
+        QuickInstaller,
+        GenericSetup,
+        Content,
+        Users,
+        I18N,
+        MockMailHost,
+        Zope2ServerRemote,
+    ),
+    name="RemoteLibraryBundle:RobotRemote",
+)
+
+ROBOT_TESTING = FunctionalTesting(
+    bases=(
+        ROBOT_FIXTURE,
+        REMOTE_LIBRARY_BUNDLE_FIXTURE,
+        WSGI_SERVER_SINGLE_THREADED_FIXTURE,
+    ),
+    name="Website:Robot",
+)
+
+
+ANSWERS_EXAMPLE_CONTENT = {
+    **DEFAULT_ANSWERS,
+    "setup_content": True,
+}
+
+
+class ExampleContentFixture(BaseFixture):
+    sites = (("kitconcept-website", ANSWERS_EXAMPLE_CONTENT),)
+
+
+EXAMPLE_CONTENT_FIXTURE = ExampleContentFixture()
+
+
+class A11YMockMailHostLayer(BaseMailLayer):
+    defaultBases = (EXAMPLE_CONTENT_FIXTURE,)
+
+
+A11Y_MOCK_MAILHOST_FIXTURE = A11YMockMailHostLayer()
+
+
+class A11YFixture(PloneRobotFixture):
+    """Accessibility testing fixture, using robot framework, for kitconcept.website."""
+
+    defaultBases = (A11Y_MOCK_MAILHOST_FIXTURE,)
+
+    def setUpPloneSite(self, portal):
+        super().setUpPloneSite(portal)
+
+
+A11Y_FIXTURE = A11YFixture()
+
+A11Y_REMOTE_LIBRARY_BUNDLE_FIXTURE = RemoteLibraryLayer(
+    bases=(EXAMPLE_CONTENT_FIXTURE,),
+    libraries=(
+        AutoLogin,
+        QuickInstaller,
+        GenericSetup,
+        Content,
+        Users,
+        I18N,
+        MockMailHost,
+        Zope2ServerRemote,
+    ),
+    name="RemoteLibraryBundle:A11YRemote",
+)
+
+A11Y_TESTING = FunctionalTesting(
+    bases=(
+        A11Y_FIXTURE,
+        A11Y_REMOTE_LIBRARY_BUNDLE_FIXTURE,
+        WSGI_SERVER_SINGLE_THREADED_FIXTURE,
+    ),
+    name="Website:A11Y",
 )
