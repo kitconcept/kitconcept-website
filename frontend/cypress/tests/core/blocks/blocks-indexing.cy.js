39bb3db103085d63b7980886e861f15e87991122
Victor Fernandez de Alba <sneridagh@gmail.com>
2026-05-29 17:03:15 +0200

    Added missing tests

---
+++ frontend/cypress/tests/core/blocks/blocks-indexing.cy.js
@@ -0,0 +1,33 @@
+describe('Block Indexing Tests', () => {
+  beforeEach(() => {
+    cy.intercept('GET', `/**/*?expand*`).as('content');
+    cy.intercept('GET', '/**/Document').as('schema');
+    cy.autologin();
+    cy.createContent({
+      contentType: 'Document',
+      contentId: 'my-page',
+      contentTitle: 'My Page',
+    });
+    cy.visit('/my-page');
+    cy.wait('@content');
+    cy.navigate('/my-page/edit');
+    cy.wait('@schema');
+  });
+
+  it('Index Text Block', () => {
+    // GIVEN: A page with a text block with the content 'Noam Avram Chomsky'
+    cy.getSlateEditorAndType('Noam Avram Chomsky').contains(
+      'Noam Avram Chomsky',
+    );
+    cy.get('#toolbar-save').click();
+
+    cy.visit('/my-page');
+    // WHEN: I search for Avram
+    cy.get('.header-intranet  .searchbox input[title="Search"]')
+      .should('not.be.disabled')
+      .type('Avram{enter}');
+
+    // THEN: The search results should contain the page 'Noam Avram Chomsky'
+    cy.get('#content-core').contains('My Page');
+  });
+});
