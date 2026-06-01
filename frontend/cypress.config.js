96aea2cfb1faa2e21069b83ab5ac2a059896955e
Víctor Fernández de Alba <sneridagh@gmail.com>
2026-06-01 15:34:13 +0200

    Update volto and add cypress to CI (#30)
    * Update to latest VLT
    
    * Added cypress tests
    
    * Mirror intranet
    
    * Fix CI
    
    * Acceptance like intra
    
    * Added missing tests
    
    * Last things standing
    
    * Adjustments
    
    * Fix tests
    
    * Remove non-sense tests
    
    * Fix acceptance tests
    
    * Disable a11y for now
    
    * Changelog
    
    * Changelog
    
    * Changelog

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('cypress');
+const path = require('path');
+
+const currentDir = path.dirname(__filename);
 
 module.exports = defineConfig({
   viewportWidth: 1280,
@@ -6,8 +9,21 @@ module.exports = defineConfig({
   retries: {
     runMode: 3,
   },
+  screenshotsFolder: `${currentDir}/cypress/screenshots`,
+  videosFolder: `${currentDir}/cypress/videos`,
+  video: true,
   e2e: {
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/tests/**/*.cy.{js,jsx,ts,tsx}',
+    supportFolder: `${currentDir}/cypress/support`,
+    supportFile: `${currentDir}/cypress/support/e2e.js`,
+    setupNodeEvents(on, config) {
+      on('task', {
+        table(message) {
+          console.table(message);
+          return null;
+        },
+      });
+    },
   },
 });
---
+++ frontend/cypress/tests/a11y/gridListingBlock.cy.js
@@ -0,0 +1,27 @@
+describe('a11y tests', () => {
+  beforeEach(() => {
+    cy.intercept('GET', `/**/*?expand*`).as('content');
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('/');
+    cy.wait('@content');
+  });
+
+  // grid block listing
+  it('Grid-Block Listing (/features/block/grid-block/listing)', () => {
+    cy.navigate('/features/block/grid-block/listing');
+    cy.wait('@content').its('response.statusCode').should('eq', 200);
+    cy.injectAxe();
+    cy.configureAxe({
+      rules: [
+        {
+          id: 'landmark-unique',
+          enabled: false,
+        },
+      ],
+    });
+    cy.checkAccessibility();
+  });
+});
