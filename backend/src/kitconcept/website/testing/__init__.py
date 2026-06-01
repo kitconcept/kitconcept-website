88ac4b55cea563eb608eb2b74b392a8f41472cef
Victor Fernandez de Alba <sneridagh@gmail.com>
2026-06-01 12:02:16 +0200

    Fix tests

---
+++ backend/src/kitconcept/website/testing/__init__.py
@@ -0,0 +1,12 @@
+from kitconcept.website.testing.a11y import A11Y_TESTING
+from kitconcept.website.testing.base import FUNCTIONAL_TESTING
+from kitconcept.website.testing.base import INTEGRATION_TESTING
+from kitconcept.website.testing.robot import ROBOT_TESTING
+
+
+__all__ = [
+    "A11Y_TESTING",
+    "FUNCTIONAL_TESTING",
+    "INTEGRATION_TESTING",
+    "ROBOT_TESTING",
+]
--- frontend/cypress/support/e2e.js
+++ frontend/cypress/support/e2e.js
@@ -6,6 +6,10 @@ import { setup, teardown } from '@plone/volto/cypress/support/reset-fixture';
 
 beforeEach(function () {
   cy.log('Setting up API fixture');
+  cy.setCookie('confirm_cookies', '1');
+  cy.setCookie('confirm_tracking', '1');
+  cy.setCookie('confirm_facebook', '1');
+  cy.setCookie('confirm_youtube', '1');
   if (!Cypress.env('a11y')) {
     setup();
   }
