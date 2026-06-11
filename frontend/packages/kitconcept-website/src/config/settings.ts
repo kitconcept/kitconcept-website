41933a30bd77c81ee521aaf3ffbf3ce7682d2125
Víctor Fernández de Alba <sneridagh@gmail.com>
2026-06-11 12:20:34 +0200

    Revert "Add backend for DSVGO banner settings (#29)" (#38)
    This reverts commit 661b265c04c07572dcaa9d65540078af2254bf27.

--- frontend/packages/kitconcept-website/src/config/settings.ts
+++ frontend/packages/kitconcept-website/src/config/settings.ts
@@ -1,17 +1,7 @@
 import { ConfigType } from '@plone/registry';
-import newsSVG from '@plone/volto/icons/news.svg';
 
 export default function install(config: ConfigType) {
   config.settings.supportedLanguages = ['de', 'en'];
-
-  // Clear DSGVO add-on settings so it gets them from the `@site` endpoint
-  const dsgvoSettings = config.settings.DSGVOBanner as {
-    showBanner?: boolean;
-    modules?: Array<string>;
-  };
-  delete dsgvoSettings.showBanner;
-  delete dsgvoSettings.modules;
-  config.settings.controlPanelsIcons['dsgvo-settings'] = newsSVG;
 
   // Volto Light Theme Configuration
   config.settings.siteHeader = true;
