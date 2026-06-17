910067b5fa4371232099fd95a5561160e8cc6045
iRohitSingh <isinghrohit0@gmail.com>
2026-06-17 15:30:05 +0530

    Add Person content type styled

--- frontend/packages/kitconcept-website/src/index.ts
+++ frontend/packages/kitconcept-website/src/index.ts
@@ -1,10 +1,14 @@
 import { ConfigType } from '@plone/registry';
 import installSettings from './config/settings';
 import installBlocks from './config/blocks';
+import PersonView from './components/theme/PersonView';
 
 const applyConfig = (config: ConfigType) => {
   installSettings(config);
   installBlocks(config);
+
+  config.views.contentTypesViews.Person = PersonView;
+
   return config;
 };
 
