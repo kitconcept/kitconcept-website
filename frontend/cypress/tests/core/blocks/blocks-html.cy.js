d18e3c300b6936c4446e7d568b479c5fcc130005
Victor Fernandez de Alba <sneridagh@gmail.com>
2026-09-10 13:57:15 +0200

    Fix no longer needed tests

--- frontend/cypress/tests/core/blocks/blocks-html.cy.js
+++ frontend/cypress/tests/core/blocks/blocks-html.cy.js
@@ -16,20 +16,3 @@ describe('Table Block Tests', () => {
     cy.navigate('/my-page/edit');
     cy.wait('@schema');
   });
-
-  it('Add HTML block', () => {
-    // when I add a maps block
-    cy.addNewBlock('html');
-    cy.get(`.block.html .npm__react-simple-code-editor__textarea`).type(
-      `<pre>This is HTML</pre>`,
-    );
-    cy.get(`.block.html [aria-label="Preview"]`).click();
-    cy.get(`.block.html pre`).contains('This is HTML');
-    cy.get('#toolbar-save').click();
-    cy.wait('@content');
-    cy.url().should('eq', Cypress.config().baseUrl + '/my-page');
-
-    // Check if HTML is present in the page view
-    cy.get('#page-document pre').should('have.text', 'This is HTML');
-  });
-});
